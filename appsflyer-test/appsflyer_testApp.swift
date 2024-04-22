7f50ff05b91a9d1c708c2c6582c63df40c9102da
Dawid Drechny <ddrechny@opera.com>
2024-04-22 17:48:20 +0200

    Initial commit

---
+++ appsflyer-test/appsflyer_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  appsflyer_testApp.swift
+//  appsflyer-test
+//
+//  Created by Dawid Drechny on 22/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct appsflyer_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ lib/lib.h
@@ -0,0 +1,18 @@
+//
+//  lib.h
+//  lib
+//
+//  Created by Dawid Drechny on 22/04/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for lib.
+FOUNDATION_EXPORT double libVersionNumber;
+
+//! Project version string for lib.
+FOUNDATION_EXPORT const unsigned char libVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <lib/PublicHeader.h>
+
+
